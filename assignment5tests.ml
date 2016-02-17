0263ddbed295b57ef8f451e17342c6d893a318db
LukeLentz <lentzl17@gmail.com>
2016-02-17 12:38:30 -0500

    tests

--- assignment5tests.ml
+++ assignment5tests.ml
@@ -1,18 +1,18 @@
 (* CALCULATIONS *)
-let t8a = has_vars (Add (Var, Int 2))
-let t8b = not (has_vars (Add (Int 1, Int 2)))
+let t8a = has_vars (Add (Var, Int 2)) = true
+let t8b = not (has_vars (Add (Int 1, Int 2))) = true
+let t8c = has_vars (Parity Var) = true
 
 let t9a = count_vars (Add (Var, Int 2)) = 1
 let t9b = count_vars (Add (Int 1, Int 2)) = 0
 
 let t10a = calc_eval (Add (Var, Int 2), 3) = 5
 
-let t11a = func_of_calc (Add (Var, Int 2)) 3 = 5
+(*let t11a = func_of_calc (Add (Var, Int 2)) 3 = 5 *)
 
-let t12a = subst (Add (Var, Int 1), Mul (Var, Var)) =
-                Mul (Add (Var, Int 1), Add (Var, Int 1))
+let t12a = subst (Add (Var, Int 1), Mul (Var, Var)) = Mul (Add (Var, Int 1), Add (Var, Int 1))
 
-let t13a = power 3 = Mul (Mul (Var, Var), Var)
+let t13a = power (Var, 3) = Mul (Mul (Var, Var), Var)
 
 let t14a = term (2, 1) = Mul(Int 2, Var)
 
